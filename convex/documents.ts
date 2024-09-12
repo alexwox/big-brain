fb059dbc780287d9923dfb9c540e15626bfae8cf
alexwox <alexander.woxstrom@gmail.com>
2024-09-12 23:48:21 +0200

    Added simple authorization

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,9 +1,17 @@
 import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
+import { ConvexError } from "convex/values";
 
 export const getDocuments = query({
     async handler(ctx) {
-        return await ctx.db.query('documents').collect();
+        const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier
+
+        if (!userId) {
+            return [];
+        }
+        return await ctx.db.query('documents')
+        .withIndex('by_tokenIdentifier', (q) => q.eq('tokenIdentifier', userId))
+        .collect();
     }
 })
 
@@ -13,8 +21,15 @@ export const createDocument = mutation({
     },
 
     async handler(ctx, args) {
+        const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier
+        console.log(userId);
+        if (!userId) {
+            throw new ConvexError('Unauthorized');
+        }
+
         await ctx.db.insert('documents', {
             title: args.title,
+            tokenIdentifier: userId,
         })
     },
 })
