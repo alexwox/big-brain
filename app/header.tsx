52b2574e9a84ff5e520d812fad047821ac6470bb
alexwox <alexander.woxstrom@gmail.com>
2024-09-15 00:10:05 +0200

    fixed header

--- app/header.tsx
+++ app/header.tsx
@@ -3,16 +3,29 @@ import { Unauthenticated, Authenticated, useQuery } from "convex/react";
 import { SignInButton, UserButton } from "@clerk/nextjs";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { Button } from "@/components/ui/button";
-
+import Image from "next/image";
 export function Header() {
-    return <div> HEADER 
-      <Unauthenticated>
-        <SignInButton />
-      </Unauthenticated>
+    return <div className = "bg-slate-900 py-4"> 
+        <div className = "container mx-auto flex justify-between items-center">
+            <div className="flex items-center gap-4 text-2xl">
+                <Image src="/logo.png" width={40} height={40}
+                className="rounded"
+                alt="an image of a brain" />
+                BIGBRAIN
+            </div>
 
-      <Authenticated>
-        <UserButton />
-        <ModeToggle />
-      </Authenticated>
+            <div>  
+               <Unauthenticated>
+                    <SignInButton />
+                </Unauthenticated>
+
+                <Authenticated>
+                    <div className = "flex items-center gap-4">
+                        <ModeToggle />
+                        <UserButton />
+                    </div>
+                </Authenticated> 
+            </div> 
+        </div>
     </div>
 }
