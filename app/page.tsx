0a2b670d902973caf8bab856d89d001169367aed
alexwox <alexander.woxstrom@gmail.com>
2024-09-22 14:41:22 +0200

    Skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import { Button } from "@/components/ui/button";
 import { DocumentCard } from "@/components/ui/document-card";
 import { Grid } from "lucide-react";
 import { UploadDocumentButton } from "./upload-document-button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   const documents = useQuery(api.documents.getDocuments);
@@ -18,11 +20,29 @@ export default function Home() {
         <UploadDocumentButton />
       </div>
 
+      {documents === undefined &&
+      <div className="grid grid-cols-4 gap-4">
+        {new Array(8).fill(0).map((_, index) => (
+          <Card className="p-6 flex flex-col gap-4 justify-between">
+            <Skeleton className=" h-[20px] rounded-lg" />
+            <Skeleton className=" h-[20px] rounded-lg" />
+            <Skeleton className=" h-[20px] rounded-lg" />
+            <Skeleton className=" w-[170px] h-[40px] rounded-lg" />
+          </Card>
+        ))}
+
+
+        </div>
+      }
+
+
+      {documents && documents.length > 0 && (
       <div className="grid grid-cols-4 gap-4">
         {documents?.map((doc) => (
           <DocumentCard document={doc} />
         ))}
       </div>
+      )}
 
     </main>
   );
