1998224b19ebbd8f0f93bc2a07d5d9d6f4600ce0
alexwox <alexander.woxstrom@gmail.com>
2024-09-26 23:01:46 +0200

    search ui fixes

--- app/dashboard/search/page.tsx
+++ app/dashboard/search/page.tsx
@@ -1,43 +1,73 @@
 "use client"
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SearchForm } from "./search-form";
 import { Doc } from "@/convex/_generated/dataModel";
 import { api } from "@/convex/_generated/api";
 import Link from "next/link";
+import { url } from "inspector";
+import { NotebookPenIcon, Files } from "lucide-react";
+
+function SearchResults({ url, score, type, text }: { url: string, score: number, type: string, text: string }) {
+    return (
+        <Link href={url} >
+            <li className="whitespace-pre-line border border-gray-200 p-4 rounded-md">
+                <div className="flex items-center gap-2 items-center justify-between">
+                    <div className="font-bold flex items-center gap-2"  >
+                        {type === "Note" ? <NotebookPenIcon className="w-4 h-4" /> : <Files className="w-4 h-4"  />}
+                        {type === "Note" ? "Note" : "Document"}
+                    </div>
+                    <div className="text-gray-500 text-sm">
+                        (Relevance: {score.toFixed(2)})
+                    </div>
+                </div>
+
+                <div>
+                    {text.substring(0, 100) + "..."}
+                </div>
+            </li>
+        </Link>
+    )
+}
 
 export default function SearchPage() {
     const [results, setResults] = useState<typeof api.search.searchAction._returnType>([]);
 
+    useEffect(() => {
+        const searchResults = localStorage.getItem("searchResults")
+        if (searchResults) {
+            setResults(JSON.parse(searchResults))
+        }
+    }, [])
+
     return (
-        <div className="w-full flex flex-col gap-6">
+        <div className="w-full flex flex-col gap-6 pb-24">
             <div className=" w-full gap-6 space-y-6 font-bold text-2xl flex items-center justify-between">
                 Search
             </div>
-            <SearchForm setResults={setResults} />
+            <SearchForm setResults={(searchResults) => {
+                setResults(searchResults)
+                localStorage.setItem("searchResults", JSON.stringify(searchResults))
+            }} />
 
             <ul className="flex flex-col gap-4">
                 {results?.map((result) => {
                     if (result.type === "notes") {
-                        return <Link href={`/dashboard/notes/${result.record._id}`} >
-                            <li key={result.record._id} className="whitespace-pre-line border border-gray-200 p-4 rounded-md">
-                                Type: "note" (score: {result.score.toFixed(2)})
-                                <br />
-                                {result.record.text.substring(0, 500) + "..."}
-                            </li>
-                        </Link>
+                        return (
+                            <SearchResults
+                                type="Note"
+                                url={`/dashboard/notes/${result.record._id}`}
+                                score={result.score}
+                                text={result.record.text}
+                            />)
                     } else {
                         return (
-                            <Link href={`/dashboard/documents/${result.record._id}`}>
-                                <li key={result.record._id} className="whitespace-pre-line border border-gray-200 p-4 rounded-md">
-                                    Type: "document" (score: {result.score.toFixed(2)})
-                                    <br />
-                                    {result.record.title}
-                                    <br />
-                                    {result.record.description}
-                                </li>
-                            </Link>
-                        )
+                            <SearchResults
+                                type="Document"
+                                url={`/dashboard/documents/${result.record._id}`}
+                                score={result.score}
+                                text={result.record.title + ": " + result.record.description}
+                            />)
                     }
                 })}
             </ul>
