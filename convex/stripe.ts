b62cfc2bcbe631ca8e3f712c0a64ecbd950fdf58
alexwox <alexander.woxstrom@gmail.com>
2025-02-19 17:56:44 +0100

    trying to solve issues

--- convex/stripe.ts
+++ convex/stripe.ts
@@ -12,53 +12,53 @@ type Metadata = {
 };
 
 export const fulfill = internalAction({
-    args: { signature: v.string(), payload: v.string() },
-    handler: async (ctx, args) => {
-      const stripe = new Stripe(process.env.STRIPE_KEY!, {
-        apiVersion: "2024-09-30.acacia",
-      });
-  
-      const webhookSecret = process.env.STRIPE_WEBHOOKS_SECRET!;
-      try {
-        const event = stripe.webhooks.constructEvent(
-          args.payload,
-          args.signature,
-          webhookSecret
+  args: { signature: v.string(), payload: v.string() },
+  handler: async (ctx, args) => {
+    const stripe = new Stripe(process.env.STRIPE_KEY!, {
+      apiVersion: "2024-09-30.acacia",
+    });
+
+    const webhookSecret = process.env.STRIPE_WEBHOOKS_SECRET!;
+    try {
+      const event = stripe.webhooks.constructEvent(
+        args.payload,
+        args.signature,
+        webhookSecret
+      );
+
+      const completedEvent = event.data.object as Stripe.Checkout.Session & {
+        metadata: Metadata;
+      };
+
+      if (event.type === "checkout.session.completed") {
+        const subscription = await stripe.subscriptions.retrieve(
+          completedEvent.subscription as string
         );
-  
-        const completedEvent = event.data.object as Stripe.Checkout.Session & {
-          metadata: Metadata;
-        };
-  
-        if (event.type === "checkout.session.completed") {
-          const subscription = await stripe.subscriptions.retrieve(
-            completedEvent.subscription as string
-          );
-  
-          const userId = completedEvent.metadata.userId as Id<"users">;
-  
-          await ctx.runMutation(internal.users.updateSubscription, {
-            userId,
-            subscriptionId: subscription.id,
-            endsOn: subscription.current_period_end * 1000,
-          });
-        }
-  
-        if (event.type === "invoice.payment_succeeded") {
-          const subscription = await stripe.subscriptions.retrieve(
-            completedEvent.subscription as string
-          );
-  
-          await ctx.runMutation(internal.users.updateSubscriptionBySubId, {
-            subscriptionId: subscription.items.data[0]?.price.id,
-            endsOn: subscription.current_period_end * 1000,
-          });
-        }
-  
-        return { success: true };
-      } catch (err) {
-        console.error(err);
-        return { success: false, error: (err as { message: string }).message };
+
+        const userId = completedEvent.metadata.userId as Id<"users">;
+
+        await ctx.runMutation(internal.users.updateSubscription, {
+          userId,
+          subscriptionId: subscription.id,
+          endsOn: subscription.current_period_end * 1000,
+        });
       }
-    },
-  });
+
+      if (event.type === "invoice.payment_succeeded") {
+        const subscription = await stripe.subscriptions.retrieve(
+          completedEvent.subscription as string
+        );
+
+        await ctx.runMutation(internal.users.updateSubscriptionBySubId, {
+          subscriptionId: subscription.items.data[0]?.price.id,
+          endsOn: subscription.current_period_end * 1000,
+        });
+      }
+
+      return { success: true };
+    } catch (err) {
+      console.error(err);
+      return { success: false, error: (err as { message: string }).message };
+    }
+  },
+});
--- convex/users.ts
+++ convex/users.ts
@@ -1,132 +1,132 @@
-// import { ConvexError, v } from "convex/values";
-// import { internalMutation, internalQuery, query } from "./_generated/server";
-// import { authMutation, authQuery } from "./util";
+import { ConvexError, v } from "convex/values";
+import { internalMutation, internalQuery, query } from "./_generated/server";
+import { authMutation, authQuery } from "./util";
 
-// const FREE_CREDITS = 5;
+const FREE_CREDITS = 5;
 
-// export const getUserById = internalQuery({
-//   args: { userId: v.string() },
-//   handler: async (ctx, args) => {
-//     const user = await ctx.db
-//       .query("users")
-//       .withIndex("by_userId", (q) => q.eq("userId", args.userId))
-//       .first();
+export const getUserById = internalQuery({
+  args: { userId: v.string() },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_userId", (q) => q.eq("userId", args.userId))
+      .first();
 
-//     return user;
-//   },
-// });
+    return user;
+  },
+});
 
-// export const getProfile = query({
-//   args: { userId: v.id("users") },
-//   handler: async (ctx, args) => {
-//     const user = await ctx.db.get(args.userId);
+export const getProfile = query({
+  args: { userId: v.id("users") },
+  handler: async (ctx, args) => {
+    const user = await ctx.db.get(args.userId);
 
-//     return {
-//       name: user?.name,
-//       profileImage: user?.profileImage,
-//     };
-//   },
-// });
+    return {
+      name: user?.name,
+      profileImage: user?.profileImage,
+    };
+  },
+});
 
-// export const createUser = internalMutation({
-//   args: {
-//     email: v.string(),
-//     userId: v.string(),
-//     name: v.string(),
-//     profileImage: v.string(),
-//   },
-//   handler: async (ctx, args) => {
-//     const user = await ctx.db
-//       .query("users")
-//       .withIndex("by_userId", (q) => q.eq("userId", args.userId))
-//       .first();
+export const createUser = internalMutation({
+  args: {
+    email: v.string(),
+    userId: v.string(),
+    name: v.string(),
+    profileImage: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_userId", (q) => q.eq("userId", args.userId))
+      .first();
 
-//     if (user) return;
+    if (user) return;
 
-//     await ctx.db.insert("users", {
-//       email: args.email,
-//       userId: args.userId,
-//       profileImage: args.profileImage,
-//       credits: FREE_CREDITS,
-//       name: args.name,
-//     });
-//   },
-// });
+    await ctx.db.insert("users", {
+      email: args.email,
+      userId: args.userId,
+      profileImage: args.profileImage,
+      credits: FREE_CREDITS,
+      name: args.name,
+    });
+  },
+});
 
-// export const updateUser = internalMutation({
-//   args: { userId: v.string(), name: v.string(), profileImage: v.string() },
-//   handler: async (ctx, args) => {
-//     const user = await ctx.db
-//       .query("users")
-//       .withIndex("by_userId", (q) => q.eq("userId", args.userId))
-//       .first();
+export const updateUser = internalMutation({
+  args: { userId: v.string(), name: v.string(), profileImage: v.string() },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_userId", (q) => q.eq("userId", args.userId))
+      .first();
 
-//     if (!user) {
-//       throw new ConvexError("user not found");
-//     }
+    if (!user) {
+      throw new ConvexError("user not found");
+    }
 
-//     await ctx.db.patch(user._id, {
-//       name: args.name,
-//       profileImage: args.profileImage,
-//     });
-//   },
-// });
+    await ctx.db.patch(user._id, {
+      name: args.name,
+      profileImage: args.profileImage,
+    });
+  },
+});
 
-// export const updateSubscription = internalMutation({
-//   args: {
-//     subscriptionId: v.string(),
-//     userId: v.id("users"),
-//     endsOn: v.number(),
-//   },
-//   handler: async (ctx, args) => {
-//     const user = await ctx.db
-//       .query("users")
-//       .withIndex("by_userId", (q) => q.eq("userId", args.userId))
-//       .first();
+export const updateSubscription = internalMutation({
+  args: {
+    subscriptionId: v.string(),
+    userId: v.id("users"),
+    endsOn: v.number(),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_userId", (q) => q.eq("userId", args.userId))
+      .first();
 
-//     if (!user) {
-//       throw new Error("no user found with that user id");
-//     }
+    if (!user) {
+      throw new Error("no user found with that user id");
+    }
 
-//     await ctx.db.patch(user._id, {
-//       subscriptionId: args.subscriptionId,
-//       endsOn: args.endsOn,
-//     });
-//   },
-// });
+    await ctx.db.patch(user._id, {
+      subscriptionId: args.subscriptionId,
+      endsOn: args.endsOn,
+    });
+  },
+});
 
-// export const updateSubscriptionBySubId = internalMutation({
-//   args: { subscriptionId: v.string(), endsOn: v.number() },
-//   handler: async (ctx, args) => {
-//     const user = await ctx.db
-//       .query("users")
-//       .withIndex("by_subscriptionId", (q) =>
-//         q.eq("subscriptionId", args.subscriptionId)
-//       )
-//       .first();
+export const updateSubscriptionBySubId = internalMutation({
+  args: { subscriptionId: v.string(), endsOn: v.number() },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_subscriptionId", (q) =>
+        q.eq("subscriptionId", args.subscriptionId)
+      )
+      .first();
 
-//     if (!user) {
-//       throw new Error("no user found with that user id");
-//     }
+    if (!user) {
+      throw new Error("no user found with that user id");
+    }
 
-//     await ctx.db.patch(user._id, {
-//       endsOn: args.endsOn,
-//     });
-//   },
-// });
+    await ctx.db.patch(user._id, {
+      endsOn: args.endsOn,
+    });
+  },
+});
 
-// export const getMyUser = authQuery({
-//   args: {},
-//   async handler(ctx, args) {
-//     return ctx.user;
-//   },
-// });
+export const getMyUser = authQuery({
+  args: {},
+  async handler(ctx, args) {
+    return ctx.user;
+  },
+});
 
-// export const updateMyUser = authMutation({
-//   args: { name: v.string() },
-//   async handler(ctx, args) {
-//     await ctx.db.patch(ctx.user._id, {
-//       name: args.name,
-//     });
-//   },
-// });
+export const updateMyUser = authMutation({
+  args: { name: v.string() },
+  async handler(ctx, args) {
+    await ctx.db.patch(ctx.user._id, {
+      name: args.name,
+    });
+  },
+});
